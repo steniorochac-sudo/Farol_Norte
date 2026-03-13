9271da380eeec51271a55f663b36905229e7d8ca
steniorochac-sudo <steniorochac@gmail.com>
2026-03-12 21:22:54 -0300

    Agora sim essa é a primeirissima versão estável do Farol Norte.

--- Farol_Norte_v01.01.001/src/main.jsx
+++ Farol_Norte_v01.01.001/src/main.jsx
@@ -10,10 +10,6 @@ import { ThemeProvider } from './context/ThemeContext.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css' 
 import 'bootstrap-icons/font/bootstrap-icons.css'
 import './index.css'
-import { loadSavedTheme } from './utils/themeManager.js' // 1. IMPORTA THEME MANAGER
-
-// 2. CHAMA O CARREGAMENTO DO TEMA SALVO ANTES DE RENDERIZAR O APP
-loadSavedTheme();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
